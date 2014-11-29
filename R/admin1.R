8db2c16709a8554403d0bc68dad686455bdcc20d
Ari Lamstein <alamstein@trulia.com>
2014-11-30 08:56:35 +0900

    admin1.region support
    data.frame and function to access.

--- R/admin1.R
+++ R/admin1.R
@@ -1,5 +1,5 @@
 if (base::getRversion() >= "2.15.1") {
-  utils::globalVariables(c("long", "lat", "group", "admin1.map"))
+  utils::globalVariables(c("long", "lat", "group", "admin1.map", "admin1.regions"))
 }
 #' An Administrative Level 1 map of every country in the world
 #' 
@@ -32,6 +32,15 @@ if (base::getRversion() >= "2.15.1") {
 #' }
 NULL
 
+#' Names of all (country, admin1) pairs on the admin1.map data.frame. 
+#' @name admin1.regions
+#' @usage data(admin1.regions)
+#' @docType data
+#' @examples
+#' data(admin1.regions)
+#' head(admin1.regions)
+NULL
+
 #' Render an Administrative Level 1 map for a specified country
 #' 
 #' Uses the map ?admin1.map.
@@ -58,4 +67,20 @@ render_admin1 = function(country.name="Japan")
   ggplot(country.map, aes(long, lat, group=group)) + 
     geom_polygon() +
     ggtitle(title)
+}
+
+#' Get all admin1 regions names for a given country
+#' 
+#' @param country.name The name of the country you want the admin1 region names of.
+#' @export
+#' @examples
+#' get_admin1_regions("Japan")
+#' get_admin1_regions("Canada")
+#' @seealso admin1.map admin1.regions render_admin1
+get_admin1_regions = function(country.name="Japan")
+{
+  data(admin1.regions, package="choroplethrMaps", envir=environment())
+  stopifnot(country.name %in% unique(admin1.regions$country))
+  
+  admin1.regions[admin1.regions$country==country.name,]  
 }
